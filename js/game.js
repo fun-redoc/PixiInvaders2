9dacc2dab4e923853c465dbf9f51f8725ced07db
Roland <fun.redoc@gmail.com>
2016-07-02 09:57:39 +0200

    refactorig

--- js/game.js
+++ js/game.js
@@ -12,6 +12,33 @@ requirejs(["jquery","PIXI",
   
   var GameMain = utils.extend(GameObject, function() {
     GameObject.call(this);
+    var invaders = new Invaders(this, 10, 10, 40, 20, 4,3);
+    var defender = new Defender(this, (windowWidth-40)/2,windowHeight-40,40,20,0xFF0000);
+    this.addDefender(defender);
+    this.addInvaders(invaders);
+
+// TODO
+// 3. invaders should be able to shoot
+// 2. when invades hit defender game over
+// 4. when an invader hits defender game over 
+// 6. when last invader killed show Won
+
+// input
+//$(document).keydown(function(event){
+// vim like left=h right=l, space to shoot
+    var keyActions = {
+      72: defender.moveLeft.bind(defender),
+      76: defender.moveRight.bind(defender),
+      32: defender.shoot.bind(defender)
+    };  
+    document.onkeydown = function(event) {
+        var event   = event || window.event;
+        var action = keyActions[event.keyCode];
+      if( action ) {
+        action();
+        event.preventDefault();
+      }
+    };
   });
   GameMain.prototype.checkInvadersHit = function() {
     if(!this.invaders) return;
@@ -34,34 +61,6 @@ requirejs(["jquery","PIXI",
    }.bind(this));
   };
 
-var stage = new GameMain();
-var invaders = new Invaders(stage, 10, 10, 40, 20, 4,3);
-var defender = new Defender(stage, (windowWidth-40)/2,windowHeight-40,40,20,0xFF0000);
-stage.addDefender(defender);
-stage.addInvaders(invaders);
-
-// TODO
-// 3. invaders should be able to shoot
-// 2. when invades hit defender game over
-// 4. when an invader hits defender game over 
-// 6. when last invader killed show Won
-
-// input
-//$(document).keydown(function(event){
-// vim like left=h right=l, space to shoot
-var keyActions = {
-  72: defender.moveLeft.bind(defender),
-  76: defender.moveRight.bind(defender),
-  32: defender.shoot.bind(defender)
-};
-document.onkeydown = function(event) {
-  var event   = event || window.event;
-  var action = keyActions[event.keyCode];
-  if( action ) {
-    action();
-    event.preventDefault();
-  }
-};
 
 // initialize renderer
 var renderer = new PIXI.autoDetectRenderer(windowWidth,windowHeight,{transparent:true, antialias: true });
@@ -74,10 +73,11 @@ $("body").append(renderer.view);
 
 
 // test
-defender.shoot();
-defender.moveRight();
+//defender.shoot();
+//defender.moveRight();
 var cout = 1;
 
+var stage = new GameMain();
 // game loop
 var animate = function() {
   requestAnimationFrame(animate);
